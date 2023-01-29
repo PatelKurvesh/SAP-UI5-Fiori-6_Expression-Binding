54386a9622b505810f0f6caa6ad395de13d7ae95
PatelKurvesh <patelkurvesh1999@gmail.com>
2023-01-29 21:47:45 +0530

    webapp

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,9 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function(Controller) {
+	"use strict";
+
+	return Controller.extend("GithubSAP-UI5-Fiori-6_Expression-Binding.controller.View1", {
+
+	});
+});
