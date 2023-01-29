66aacc17cfac2dffac7a6546ef0eb0f8ef47368a
PatelKurvesh <patelkurvesh1999@gmail.com>
2023-01-30 01:45:26 +0530

    Updated View and Controller

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -4,6 +4,12 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("GithubSAP-UI5-Fiori-6_Expression-Binding.controller.View1", {
+		
+		onInit : function(){
+			var oModel = new sap.ui.model.json.JSONModel();
+			oModel.loadData("model/car.json");
+			this.getView().setModel(oModel);
+		}
 
 	});
 });
